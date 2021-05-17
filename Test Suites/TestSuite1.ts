387aafd79ab27b6cbd85959bac239cce467c7e9a
divine-moment-of-truth <shortzenhausen@gmail.com>
2021-05-17 19:47:07 +0100

    Added test case 1 and 2 code

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>df36f577-7b38-4158-860b-535428728303</testSuiteGuid>
+   <testCaseLink>
+      <guid>6c40beee-4db6-487c-90be-24adef66993d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test 1 by Tom</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
